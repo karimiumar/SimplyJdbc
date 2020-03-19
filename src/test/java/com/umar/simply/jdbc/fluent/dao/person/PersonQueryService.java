e38706c9176d5f3b22f228d56c1bdf62ef61631a
karimiumar <karimiumar@gmail.com>
2020-03-19 21:21:16 +0530

    Issue | Cosmetic Changes & Code Refactoring #6

--- src/test/java/com/umar/simply/jdbc/fluent/dao/person/PersonQueryService.java
+++ src/test/java/com/umar/simply/jdbc/fluent/dao/person/PersonQueryService.java
@@ -2,6 +2,7 @@ package com.umar.simply.jdbc.fluent.dao.person;
 
 import com.umar.simply.jdbc.fluent.dao.QueryService;
 import java.util.List;
+import static com.umar.simply.jdbc.meta.ColumnValue.set;
 import static com.umar.simply.jdbc.fluent.dao.person.PersonTable.PERSON_ROW_MAPPER;
 import static com.umar.simply.jdbc.fluent.dao.person.PersonTable.*;
 import com.umar.simply.jdbc.fluent.dao.person.contract.FluentPersonQueryService;
@@ -31,22 +32,20 @@ public class PersonQueryService extends QueryService implements FluentPersonQuer
     public List<Person> findByName(String name) {
         Objects.requireNonNull(name, "parameter <name> is required");
         List<Person> result = Arrays.asList();
-        if(name.equals(" ") || name.isEmpty()) return result;
+        if(name.isBlank() || name.isEmpty()) return result;
         String [] names = name.trim().split("\\s");     
         if(names.length > 1) {
-            String firstName = names[0];
-            String lastName = names[1];
-            result = searchByName(result, firstName, lastName);
+           return searchByLastOrFirstNames(names);
         } else {
-            result = searchByName(result, name, name);
+            result = searchByName(result, name);
         }
         return result;
     }
 
-    private List<Person> searchByName(List<Person> result, String firstName, String lastName) {
-        result = searchCriteria(firstName);
+    private List<Person> searchByName(List<Person> result, String name) {
+        result = searchCriteria(name);
         if(result.isEmpty()) {
-            result = searchCriteria(lastName);
+            result = searchCriteria(name);
         }
         return result;
     }
@@ -57,6 +56,11 @@ public class PersonQueryService extends QueryService implements FluentPersonQuer
         if(result.isEmpty()) {
             result = SELECT().ALL().FROM(TBL_PERSON).WHERE().COLUMN(PERSON_LAST_NAME).LIKE(nameCriteria).using(PERSON_ROW_MAPPER).execute();
         }
+        return result;
+    }
+    
+    private List<Person> searchByLastOrFirstNames(String ... names) {
+        List<Person> result = SELECT().ALL().FROM(TBL_PERSON).WHERE().COLUMN(PERSON_FIRST_NAME).LIKE("%"+names[0]+"%").OR().COLUMN(PERSON_LAST_NAME).LIKE("%"+names[1]+"%").using(PERSON_ROW_MAPPER).execute();
         return result;
     }
 }
